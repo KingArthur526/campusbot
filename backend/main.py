005e47b0ca7a3d73d61e927732a6756f6721cf88
Vineeth <vineethpam@gmail.com>
2025-08-05 15:49:12 +0530

    Day 3 Had a lot of difficulty setting up a connection between ollama and fastapi, but solved the problem which lay in the formatting of dictionaries. Done using the Basemodel object in the pydantic module

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import httpx
+
 app = FastAPI()
+
 @app.get("/ping")
 def ping():
     return {"Shraddha":"sleeping"}
+
+class Ask(BaseModel):
+    question: str
+
+@app.post("/chat")
+async def chat(body: Ask):
+    payload = {
+        "model":"llama3.2",
+        "prompt":body.question,
+        "stream":False
+        }
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(
+            "http://localhost:11434/api/generate",
+            json = payload,
+            timeout = 30,
+            )
+    
+    answer = resp.json().get("response", "")
+    return {"answer": answer}
+    
